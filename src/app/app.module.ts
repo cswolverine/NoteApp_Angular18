f118c3b0709c72f2b301f0c63b805b8fef249a6f
cswolverine <csgurjar2604@gmail.com>
2024-07-17 10:42:05 +0530

    initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { FormsModule } from '@angular/forms';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    DashboardComponent,
+  
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,FormsModule,HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/service/crud.service.ts
@@ -0,0 +1,14 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CrudService {
+
+  serviceURL:string='';
+
+  constructor(private http: HttpClient) {
+    this.serviceURL='http://localhost:3000/tasks';
+   }
+}
